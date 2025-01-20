6cfaf36ca5ec7fda0f8477932eaa036c41bfb4d9
aosmanovv <a_osmanov@kbtu.kz>
2025-01-20 16:40:20 +0500

    booleans done

---
+++ booleans.py
@@ -0,0 +1,24 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+def myFunction() :
+    return True
+
+print(myFunction())
+if myFunction():
+    print("YES!")
+else:
+    print("NO!")
+
+x = 200
+print(isinstance(x, int))
